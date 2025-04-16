c6b47226e9479a6030d50fb160d7bf690df55996
rochakkadel <rochakrajkadel@gmail.com>
2025-04-16 11:23:32 -0700

    done

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,13 @@
+import java.util.concurrent.CountDownLatch;
+
+
 public class Student {
     Name name;
     String major;
     int studentID;
     double gpa;
     Course[] coursesList;
+
 
     public Student(Name name, String major, int studentID, double gpa, Course[] coursesList) {
         this.name = name;
@@ -13,59 +17,80 @@ public class Student {
         this.coursesList = coursesList;
     }
 
+
     public Student(Name name) {
         this.name = name;
     }
 
+
     public Student() {
 
+
     }
+
 
     /**
      * 1 point for attempt. 2 points for correct implementation.
      * TODO: Get the number of units this student is taking.
      */
     public int getUnits() {
-        return 0;
+        int totalUnits = 0;
+        if (coursesList != null) {
+            for (Course course : coursesList) {
+                totalUnits += course.getUnits();
+            }
+        }
+        return totalUnits;
     }
+
 
     public Name getName() {
         return name;
     }
 
+
     public void setName(Name name) {
         this.name = name;
     }
+
 
     public String getMajor() {
         return major;
     }
 
+
     public void setMajor(String major) {
         this.major = major;
     }
+
 
     public int getStudentID() {
         return studentID;
     }
 
+
     public void setStudentID(int studentID) {
         this.studentID = studentID;
     }
+
 
     public double getGpa() {
         return gpa;
     }
 
+
     public void setGpa(double gpa) {
         this.gpa = gpa;
     }
+
 
     public Course[] getCoursesList() {
         return coursesList;
     }
 
+
     public void setCoursesList(Course[] coursesList) {
         this.coursesList = coursesList;
     }
 }
+
